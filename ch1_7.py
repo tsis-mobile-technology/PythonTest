8784dd2fe9759b1f13bb016003260f032ca7e7ae
proidea <gainworld@gmail.com>
2019-02-01 16:02:19 +0900

    chapter 1 example add, sample blockchain, all link scraping

--- ch1_7.py
+++ ch1_7.py
@@ -1,7 +1,9 @@
 
 from bs4 import BeautifulSoup
 import urllib.request as req
+from urllib.parse import urljoin
 import sys
+import re
 
 url = "https://ko.wikipedia.org/wiki/%EC%9C%A4%EB%8F%99%EC%A3%BC"
 #res = urllib.urlopen(url)
@@ -15,6 +17,22 @@ print(a_list)
 #reload(sys)
 #sys.setdefaultencoding('utf-8')
 #print(sys.getdefaultencoding())
+
 for a in a_list:
 	name = a.string
 	print("-", name)
+#CSS 선택자로 추출하는 예제
+sel =  lambda q : print("24>", soup.select_one(q).string)
+sel("#mw-head #p-personal-label")
+
+#정표현식과 조립하여 추출 예제
+li = soup.find_all(href=re.compile(r"^http*://"))
+for e in li:
+	print("30>", e.attrs['href'])
+
+#상대경로 처리 방법
+aa = soup.select("a[href]")
+for a in aa:
+	#print("36>", a.attrs['href'] )
+	bb = urljoin(url, a.attrs['href'])
+	print("38>",bb)
